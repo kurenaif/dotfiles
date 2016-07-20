af96efaed30e892a146356655f87919c970707ab
kurenaif <kurenaif>
2016-07-20 09:53:21 -0700

    commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/
+cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt_pdf
+cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt_tex
+cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt
+
+cp ./bin/makeplt /usr/local/bin/
+cp ./bin/makeplt_eps /usr/local/bin/
+cp ./bin/makeplt_pdf /usr/local/bin/
+cp ./bin/makeplt_tex /usr/local/bin/
+cp ./bin/git-echo-branch-tmux-current-pane /usr/local/bin/
