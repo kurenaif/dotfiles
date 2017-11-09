1802b74a3dc603f85963a22f975b800b5c0f0d78
kurenaif <antyobido@gmail.com>
2017-11-09 14:00:37 +0900

    adjust install.shj

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
-cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/
-cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt_pdf
-cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt_tex
-cp ./bin/bash_completion/makeplt_eps /etc/bash_completion.d/makeplt
-
-cp ./bin/makeplt /usr/local/bin/
-cp ./bin/makeplt_eps /usr/local/bin/
-cp ./bin/makeplt_pdf /usr/local/bin/
-cp ./bin/makeplt_tex /usr/local/bin/
-cp ./bin/git-echo-branch-tmux-current-pane /usr/local/bin/
+cp ./fish_pronpt.fish ~/.config/fish/functions
+cp informative_git_prompt/__informative_git_prompt.fish ~/.config/fish/functions
+ln -s .gitconfig ~/.gitconfig
