2088b781b66107ff7d112747c28648124af446bf
kurenaif <antyobido@gmail.com>
2018-06-02 19:34:18 +0900

    update install

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
-cp ./fish_pronpt.fish ~/.config/fish/functions
-cp informative_git_prompt/__informative_git_prompt.fish ~/.config/fish/functions
-ln -s .gitconfig ~/.gitconfig
+set -ux
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# fish
+# mkdir -p ~/.config/fish/functions
+ln -s ${SCRIPT_DIR}/fish_prompt.fish ~/.config/fish/functions 
+ln -s ${SCRIPT_DIR}/informative_git_prompt/__informative_git_prompt.fish ~/.config/fish/functions 
+
+# git
+ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig 
+
+# bash
+
+echo "-------my setting--------" >> ~/.bashrc
+echo "export $SCRIPT_DIR/.bashrc" >> ~/.bashrc
